2cc9609cc2c945d8cf389027a40779b5ef2dc886
Alfar <https://github.com/AlfarLatypov/reposit.git>
2017-12-13 21:02:24 +0600

    reinterpret_cast

--- 12.12.2017/Source.cpp
+++ 12.12.2017/Source.cpp
@@ -3,27 +3,43 @@
 #include <string>
 using namespace  std;
 
+class MyException : public exception {
+public:
+	virtual const char * what() const {
+		return "Overfow exception!!";
+	}
 
+};
 
 
 int convert(string s) {
-	int res = 0, radix = 1;
-	for (int i = s.length() - 1; i >= 0; i--) {
-		res += radix * ((int)s[i] - 48);
-		radix *= 10;
+	if (s.length() > 31) {
+		throw MyException();
 	}
-	return res;
+		int res = 0, radix = 1;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			res += radix * ((int)s[i] - 48);
+			radix *= 10;
+		}
+		return res;
+	
 }
 
 
 void main() {
 	
-	string a = "128";
+	/*string a = "128";
 	cout << convert(a) << endl;
 
-
 	int x = stoi(a);
-	cout << x << endl;
+	cout << x << endl;*/
+		
+	try {
+		convert("3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333");
+	}
+	catch (MyException e) {
+		cout << e.what() << endl;
+	}
 }
 
 
